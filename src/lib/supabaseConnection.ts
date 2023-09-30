d8a6a3b1242912aa3e3f3e954f2ab9103718297d
Chris Tague <cstague6401@gmail.com>
2023-09-30 17:45:25 -0500

    adding dope types

--- src/lib/supabaseConnection.ts
+++ src/lib/supabaseConnection.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
+import type {Database} from './supabase';
 
 const SUPABASE_URL = process.env.SUPABASE_URL || 'Please add SUPABASE_URL to your .env file';
 const SUPABASE_PUBLIC_API_KEY = process.env.SUPABASE_PUBLIC_API_KEY || 'Please add SUPABASE_URL to your .env file';
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_API_KEY);
+const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLIC_API_KEY);
 
 export default supabase;
