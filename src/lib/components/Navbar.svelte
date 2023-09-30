bb189d1f1c2619f7c4d5ae78c7c994c29f636cad
jorgoose <78634823+jorgoose@users.noreply.github.com>
2023-09-30 15:49:52 -0500

    Lookin good

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher<{ navigate: { page: string } }>();
+
+    function navigateTo(page: string) {
+        dispatch('navigate', { page });
+    }
+</script>
+
+<style>
+    /* Styling for the navbar */
+    .navbar {
+        background-color: #fff;
+        box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1);
+    }
+    .nav-icon {
+        font-size: 1.5rem;
+    }
+</style>
+
+<div class="navbar fixed bottom-0 left-0 w-full z-10 flex items-center justify-around p-4">
+    <button class="nav-icon" on:click={() => navigateTo('search')}>
+        <i class="fas fa-search"></i>
+    </button>
+    <button class="nav-icon" on:click={() => navigateTo('add')}>
+        <i class="fas fa-plus"></i>
+    </button>
+    <button class="nav-icon" on:click={() => navigateTo('profile')}>
+        <i class="fas fa-user"></i>
+    </button>
+    <button class="nav-icon" on:click={() => navigateTo('inbox')}>
+        <i class="fas fa-envelope"></i>
+    </button>
+    <button class="nav-icon" on:click={() => navigateTo('settings')}>
+        <i class="fas fa-cog"></i>
+    </button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,66 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script lang="ts">
+  import Navbar from "$lib/components/Navbar.svelte";
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
+  // Hardcoded contractors list for demonstration purposes
+  let contractors = [
+      {
+          id: 1,
+          name: "John Doe",
+          specialty: "John Deere Tractors",
+          location: "Texas, USA",
+          rating: 4.5,
+          image: "path_to_image_1.jpg"
+      },
+      {
+          id: 2,
+          name: "Jane Smith",
+          specialty: "CAT Heavy Equipment",
+          location: "California, USA",
+          rating: 4.7,
+          image: "path_to_image_2.jpg"
+      },
+      // ... Add more contractors as needed
+  ];
+
+  let currentPage: string = 'search'; // Default view
+
+function handleNavigation(event: CustomEvent<{ page: string }>) {
+    currentPage = event.detail.page;
+}
+</script>
+
+<style>
+  /* Custom styling for modern feel */
+  .card-shadow {
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   }
+    .content {
+        padding-bottom: 80px; /* Adjust depending on the height of your navbar to prevent overlap */
+    }
 </style>
+
+<div class="p-4 bg-gray-100 h-full min-h-screen content">
+    <div class="max-w-screen-lg mx-auto">
+      <input type="text" placeholder="🔍 Search Contractors..." class="p-3 bg-white border rounded-full mb-6 w-full shadow-sm focus:outline-none focus:border-indigo-300">
+
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {#each contractors as contractor}
+              <div class="bg-white rounded-lg overflow-hidden card-shadow">
+                  <img src="{contractor.image}" alt="{contractor.name}" class="w-full h-48 object-cover">
+                  <div class="p-4">
+                      <h2 class="text-xl mb-2 font-semibold">{contractor.name}</h2>
+                      <p class="text-gray-500 text-sm mb-2 truncate"><strong>Specialty:</strong> {contractor.specialty}</p>
+                      <p class="text-gray-500 text-sm mb-4 truncate"><strong>Location:</strong> {contractor.location}</p>
+                      <div class="flex items-center mb-3">
+                          <span class="text-yellow-400 mr-2">⭐ {contractor.rating}</span>
+                          <span class="text-gray-600 text-sm">({contractor.rating} Rating)</span>
+                      </div>
+                      <button class="bg-indigo-500 hover:bg-indigo-600 text-white p-2 rounded-full w-full text-center">Contact</button>
+                  </div>
+              </div>
+          {/each}
+      </div>
+  </div>
+</div>
+
+<Navbar on:navigate={handleNavigation} />
