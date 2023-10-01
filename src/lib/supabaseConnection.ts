39c5ea2a70e2ffaa8b26ff051f501a22b0943a47
Kyle Goben <kygoben@gmail.com>
2023-09-30 20:57:15 -0500

    remove old supabase connection

--- src/lib/supabaseConnection.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import type {Database} from './supabase';
-
-const SUPABASE_URL = process.env.SUPABASE_URL || 'Please add SUPABASE_URL to your .env file';
-const SUPABASE_PUBLIC_API_KEY = process.env.SUPABASE_PUBLIC_API_KEY || 'Please add SUPABASE_URL to your .env file';
-
-const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLIC_API_KEY);
-
-export default supabase;
